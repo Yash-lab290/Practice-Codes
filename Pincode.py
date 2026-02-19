d0d1d989613a813f57c7d81e1ae7bbcdf99148ed
Yash <yashbhoi2801@gmail.com>
2026-02-19 23:32:19 +0530

    Add files via upload

---
+++ Pincode.py
@@ -0,0 +1,15 @@
+# write a program to input an address containing a pincode and extract and print the pincode from the input address
+add=input ("Enter text :")
+i=0 
+ln=len(add)
+# for ch in add:
+while i<ln:
+    ch=add[i]
+    if ch.isdigit():
+        sub=add[i:(i+6)]
+        if sub.isdigit():
+            print(sub)
+            break
+    i+=1  
+    # Example input : New Delhi, 110001
+    # output : 110001   
