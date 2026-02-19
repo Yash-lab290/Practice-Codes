7894fe80527e8db580c6114e110a1680545b873e
Yash <yashbhoi2801@gmail.com>
2026-02-19 23:48:29 +0530

    Add New Feature

---
+++ Quadratic.py
@@ -0,0 +1,23 @@
+# Write a progrm to find th Square root of a given Quadratic Equation
+
+import math
+print("For quadratic equation, ax**2+bx+c=0,Enter cofficient below")
+a= int (input("Enter a :"))
+b= int (input("Enter b :"))
+c= int (input("Enter c :"))
+if a==0:
+    print("Value of",a,'Should not be Zero')
+    print("\n Aborting !!!!!")
+else:
+    delta = b*b-4*a*c
+    if delta>0:
+        root1=(-b+math.sqrt(delta))/(2*a)
+        root2=(-b-math.sqrt(delta))/(2*a)
+        print("Roots are REAL and UNEQUAL")
+        print("Root1 =",root1, "Root2 =",root2)
+    elif delta==0:
+        root1 = -b/(2*a)
+        print("Roots are REAL and EQUAL")
+        print("Root1=",root1,"Root2 =",root1)
+    else:
+        print("Roots are COMPLEX and IMAGINARY")
