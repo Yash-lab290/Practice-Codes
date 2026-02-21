2fe324e68ce2276d21f2dce5d413f9fffbac6a12
Yash <yashbhoi2801@gmail.com>
2026-02-22 00:12:07 +0530

    commit done

---
+++ Raw.py
@@ -0,0 +1,12 @@
+print("hiii")
+print ("what you do in the class")
+def add(a,b):
+    return a+b      
+print(add(2,3))
+def sub(a,b):   
+    return a-b        
+
+print(sub(5,3))
+
+
+#PUSH ho gai hai
