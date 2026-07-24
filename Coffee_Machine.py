10ab755fec3af9960db31e385c4d89291e4d3672
Yash <yashbhoi2801@gmail.com>
2026-07-25 01:13:08 +0530

    completed.

--- Coffee_Machine.py
+++ Coffee_Machine.py
@@ -47,8 +47,6 @@ Menu = {
     }
 }    
 
-
-
 profit = 0
 resources = {
     "water":500,
@@ -69,10 +67,23 @@ def process_coins():
     coins_twenty=int(input("How many 20rs coin?: "))
     total=coins_five*5 + coins_ten*10 + coins_twenty*20
     return total
-    
+def is_payment_successful(money_received,coffee_cost):
+    if money_received >= coffee_cost:
+        global profit
+        profit += coffee_cost
+        change = money_received-coffee_cost
+        print(f"Here is your Rs{change} in change.")
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded. ")
+        return False
+def make_coffee(coffee_name,coffee_ingredients):
+    for item in coffee_ingredients:
+        resources[item] -= coffee_ingredients[item]
+    print(f"Here is your {coffee_name} ccc..Enjoy !!")
 is_on = True
 while is_on:
-    choice = input("What would you like to have?(Latte/Espresso/cappuccino).")
+    choice = input("What would you like to have?(Latte/Espresso/Cappuccino).")
     if choice == "off":
         is_on = False
     elif choice == "report":
@@ -85,4 +96,7 @@ while is_on:
         print(coffee_type)
         if check_resources(coffee_type['ingredients']):
             payment=process_coins()
-            # 26 min se video 87
+            if is_payment_successful(payment,coffee_type['cost']):
+                make_coffee(choice,coffee_type['ingredients'])
+
+            
